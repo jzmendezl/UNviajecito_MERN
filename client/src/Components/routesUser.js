15557ed3b0ff782e664a347071f56b97bb2963f1
Joe Mendez <54820626+jzmendezl@users.noreply.github.com>
2022-10-30 21:44:21 -0500

    Validacion de añadir ruta listo

--- client/src/Components/routesUser.js
+++ client/src/Components/routesUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useUsers } from '../context/userContext'
+import { validateAddTravel } from '../helpers/validateForm'
 import '../resources/css/routesUser.css'
 
 const RoutesUser = () => {
@@ -16,6 +17,7 @@ const RoutesUser = () => {
     const [userVehicle, setUserVehicle] = useState([])
     const [vehicleToTravel, setVehicleToTravel] = useState(null)
     const [credentials, setCredentials] = useState(null)
+    const [correctForm, setCorrectForm] = useState(false)
 
     const [wheels, setWheels] = useState([])
 
@@ -23,111 +25,116 @@ const RoutesUser = () => {
         setUserVehicle(currentUser?.vehicle)
         setCredentials(getCredentials())
         setWheels(currentUser?.userWheels)
-}, [currentUser?.userWheels, currentUser?.vehicle, getCredentials])
+    }, [currentUser?.userWheels, currentUser?.vehicle, getCredentials])
 
-// eslint-disable-next-line no-unused-vars
-const [travel, setTravel] = useState(null)
+    // eslint-disable-next-line no-unused-vars
+    const [travel, setTravel] = useState(null)
 
-const updateData = async (id, filter) => {
-    try {
-      const res = await updateUser(id, filter)
-      return res
-    } catch (error) {
-      console.error({ message: error });
-    }
-  }
-
-const addNewRouteUser = async (e) => {
-    e.preventDefault()
-
-
-    const newTravel = {
-        userName: currentUser.userName,
-        contact: contactForm,
-        email: currentUser.email,
-        vehicle: userVehicle[vehicleToTravel],
-        source: source,
-        destiny: destination,
-        dateTime: Date.parse(date),
-        seats: parseInt(seats),
-        price: parseInt(price),
-        remark: remark,
-        status: 'Created'
+    const updateData = async (id, filter) => {
+        try {
+            const res = await updateUser(id, filter)
+            return res
+        } catch (error) {
+            console.error({ message: error });
+        }
     }
 
-    setTravel(newTravel)
-    const res = await addTravel(newTravel)
-    wheels.push(res.tid)
-    setCurrentUser({ ...currentUser, userWheels: wheels })
-    await updateData(credentials.UID, { userWheels: wheels })
-    // await updateUser(credentials.UID, { userWheels: wheels })
-}
+    const addNewRouteUser = async (e) => {
+        e.preventDefault()
 
-const sendRoute = () => {
-    setTimeout(() => {
-        setViewRender(false)
-    }, 1000);
-}
 
-return (
-    <div id='bodyAR'>
-        <p id='titleAR'>Agregar Nueva Ruta</p>
-        <form className='formAR' onSubmit={addNewRouteUser}>
-            <label htmlFor="placeSource">
-                <span>Origen</span>
-                <input type="text" name='placeSource' id='placeSource' onChange={e => setSource(e.target.value)} />
-            </label>
-            <label htmlFor="placeDestination">
-                <span>Destino</span>
-                <input type="text" name='placeDestination' id='placeDestination' onChange={e => setDestination(e.target.value)} />
-            </label>
-            <label htmlFor="dateTime">
-                <span>Fecha y Hora</span>
-                <input type='datetime-local' name='dateTime' id='dateTime' onChange={e => setDate(e.target.value)} />
-            </label>
-            <label htmlFor="quotas">
-                <span>Cupos</span>
-                <input type="number" min={1} max={5} name='quotas' id='quotas' onChange={e => setSeats(e.target.value)} />
-            </label>
-            <label htmlFor="price">
-                <span>Precio</span>
-                <input type='number' name='price' id='price' onChange={e => setPrice(e.target.value)} />
-            </label>
-            <label htmlFor="contactForm">
-                <span>Forma de Contacto</span>
-                <div id='contactForm'>
-                    <label htmlFor="email">
-                        <input type="radio" name="contact" id="email" value={currentUser.email} onClick={e => setContactForm(e.target.value)} />
-                        <p>Correo</p>
-                    </label>
-                    <label htmlFor="celphone">
-                        <input type="radio" name="contact" id="celphone" value={currentUser.celPhone} onClick={e => setContactForm(e.target.value)} />
-                        <p>Celular</p>
-                    </label>
-                </div>
-            </label>
-            <label htmlFor="remark">
-                <span>Observaciones</span>
-                <input type="text" name='remark' id='remark' onChange={e => setRemark(e.target.value)} />
-            </label>
-            <label htmlFor="vehicle">
-                {/* <span>Observaciones</span> */}
-                <select name="vehicle" id="vehicle" onChange={e => setVehicleToTravel(e.target.value)}>
-                    <option value="">--Please choose an option--</option>
-                    {
-                        userVehicle.map((vehicle, index) => (
-                            <option value={index} >
-                                {vehicle.plate}
-                            </option>
-                        ))
-                    }
-                </select>
+        const newTravel = {
+            userName: currentUser.userName,
+            contact: contactForm,
+            email: currentUser.email,
+            vehicle: userVehicle[vehicleToTravel],
+            source: source,
+            destiny: destination,
+            dateTime: Date.parse(date),
+            seats: parseInt(seats),
+            price: parseInt(price),
+            remark: remark,
+            status: 'Created'
+        }
 
-            </label>
-            <button type="submit" id='btnFormAR' onClick={sendRoute}>Agregar</button>
-        </form>
-    </div>
-)
+        if (validateAddTravel(newTravel)) {
+            await setCorrectForm(true)
+            setTravel(newTravel)
+            const res = await addTravel(newTravel)
+            wheels.push(res.tid)
+            setCurrentUser({ ...currentUser, userWheels: wheels })
+            await updateData(credentials.UID, { userWheels: wheels })
+        }
+        // await updateUser(credentials.UID, { userWheels: wheels })
+    }
+
+    const sendRoute = () => {
+        setTimeout(() => {
+            if (correctForm) {
+                setViewRender(false)
+            }
+        }, 500);
+    }
+    console.log(travel);
+    return (
+        <div id='bodyAR'>
+            <p id='titleAR'>Agregar Nueva Ruta</p>
+            <form className='formAR' onSubmit={addNewRouteUser}>
+                <label htmlFor="placeSource">
+                    <span>Origen</span>
+                    <input type="text" name='placeSource' id='placeSource' onChange={e => setSource(e.target.value)} />
+                </label>
+                <label htmlFor="placeDestination">
+                    <span>Destino</span>
+                    <input type="text" name='placeDestination' id='placeDestination' onChange={e => setDestination(e.target.value)} />
+                </label>
+                <label htmlFor="dateTime">
+                    <span>Fecha y Hora</span>
+                    <input type='datetime-local' name='dateTime' id='dateTime' onChange={e => setDate(e.target.value)} />
+                </label>
+                <label htmlFor="quotas">
+                    <span>Cupos</span>
+                    <input type="number" min={1} max={5} name='quotas' id='quotas' onChange={e => setSeats(e.target.value)} />
+                </label>
+                <label htmlFor="price">
+                    <span>Precio</span>
+                    <input type='number' name='price' id='price' onChange={e => setPrice(e.target.value)} />
+                </label>
+                <label htmlFor="contactForm">
+                    <span>Forma de Contacto</span>
+                    <div id='contactForm'>
+                        <label htmlFor="email">
+                            <input type="radio" name="contact" id="email" value={currentUser.email} onClick={e => setContactForm(e.target.value)} />
+                            <p>Correo</p>
+                        </label>
+                        <label htmlFor="celphone">
+                            <input type="radio" name="contact" id="celphone" value={currentUser.celPhone} onClick={e => setContactForm(e.target.value)} />
+                            <p>Celular</p>
+                        </label>
+                    </div>
+                </label>
+                <label htmlFor="remark">
+                    <span>Observaciones</span>
+                    <input type="text" name='remark' id='remark' onChange={e => setRemark(e.target.value)} />
+                </label>
+                <label htmlFor="vehicle">
+                    {/* <span>Observaciones</span> */}
+                    <select name="vehicle" id="vehicle" onChange={e => setVehicleToTravel(e.target.value)}>
+                        <option value="">--Selecciona un Vehiculo--</option>
+                        {
+                            userVehicle.map((vehicle, index) => (
+                                <option value={index} >
+                                    {vehicle.plate}
+                                </option>
+                            ))
+                        }
+                    </select>
+
+                </label>
+                <button type="submit" id='btnFormAR' onClick={sendRoute}>Agregar</button>
+            </form>
+        </div>
+    )
 }
 
 export default RoutesUser
--- client/src/helpers/validateForm.js
+++ client/src/helpers/validateForm.js
@@ -1,131 +1,245 @@
 import toast from "react-hot-toast";
 
 export const validateDomain = (email) => {
-    if (email.split('@')[1] === 'unal.edu.co') {
-      console.log('email Valido');
-      return true;
-    }
-    else {
-      toast.error('Correo Invalido!',
-        {
-          style: {
-            borderRadius: '10px',
-            background: '#282c34',
-            color: '#2ececece',
-          },
-        }
-      );
-      return false;
-    }
-  }
-
-  export const formValidate = (event) => {
-    event.preventDefault();
-    let userName = event.target[0].value;
-    // eslint-disable-next-line no-unused-vars
-    let celPhone = event.target[1].value;
-    let email = event.target[2].value;
-    // eslint-disable-next-line no-unused-vars
-    let pass = event.target[3].value;
-
-    let expRegName =  /^[a-zA-Z0-9_-\s]{3,16}$/;
-
-    if (!userName) {
-      alert('Nombre Requerido')
-      return false;
-    }
-    if (!expRegName.exec(userName)) {
-      // alert('El Campo Nombre Admite Letras Y Espacios')
-      toast.error('El Campo Nombre de Usuario Admite Letras Y Espacio',
-        {
-          style: {
-            borderRadius: '10px',
-            background: '#282c34',
-            color: '#2ececece',
-          },
-        }
-      );
-    }
-    if (validateDomain(email) === true) {
-      console.log('Enviar datos');
-    } else {
-      console.log('Solicitar nuevos datos');
-      return false;
-    }
-
+  if (email.split('@')[1] === 'unal.edu.co') {
+    console.log('email Valido');
     return true;
   }
+  else {
+    toast.error('Correo Invalido!',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    );
+    return false;
+  }
+}
 
-  export const validateFormVehicle = (vehicle) => {
-    let expRegPlateCar = /([A-Z]{3})([-]{1})([0-9]{3})/;
-    let expRegPlateBike = /([A-Z]{3})([-]{1})([0-9]{2})([A-Z]{1})/;
+export const formValidate = (event) => {
+  event.preventDefault();
+  let userName = event.target[0].value;
+  // eslint-disable-next-line no-unused-vars
+  let celPhone = event.target[1].value;
+  let email = event.target[2].value;
+  // eslint-disable-next-line no-unused-vars
+  let pass = event.target[3].value;
 
-    if (vehicle?.kind === 'Carro' && (expRegPlateCar.exec(vehicle?.plate) === null)) {
-        toast.error('El Campo Placa Debe Ser Valida',
-            {
-                style: {
-                    borderRadius: '10px',
-                    background: '#282c34',
-                    color: '#2ececece',
-                },
-            }
-        )
-        return false
-    }
+  let expRegName = /^[a-zA-Z0-9_-\s]{3,16}$/;
 
-    if (vehicle?.kind === 'Moto' && (expRegPlateBike.exec(vehicle?.plate) === null)) {
-      alert('que pasa')  
-      toast.error('El Campo Placa Debe Ser Valida',
-            {
-                style: {
-                    borderRadius: '10px',
-                    background: '#282c34',
-                    color: '#2ececece',
-                },
-            }
-        )
-        return false
-    }
+  if (!userName) {
+    alert('Nombre Requerido')
+    return false;
+  }
+  if (!expRegName.exec(userName)) {
+    // alert('El Campo Nombre Admite Letras Y Espacios')
+    toast.error('El Campo Nombre de Usuario Admite Letras Y Espacio',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    );
+  }
+  if (validateDomain(email) === true) {
+    console.log('Enviar datos');
+  } else {
+    console.log('Solicitar nuevos datos');
+    return false;
+  }
 
-    if (vehicle?.model < 1990 || vehicle?.model > 2023) {
-        toast.error('El Campo Modelo debe estar entre 1990 y 2023',
-            {
-                style: {
-                    borderRadius: '10px',
-                    background: '#282c34',
-                    color: '#2ececece',
-                },
-            }
-        )
-        return false
-    }
+  return true;
+}
 
-    if (vehicle?.kind === 'Moto' && !(parseInt(vehicle?.seats) === 1)) {
-        toast.error('El Campo Puesto Solo Puede Ser 1 Si Eliges Una Moto',
-            {
-                style: {
-                    borderRadius: '10px',
-                    background: '#282c34',
-                    color: '#2ececece',
-                },
-            }
-        )
-        return false
-    }
+export const validateFormVehicle = (vehicle) => {
+  let expRegPlateCar = /([A-Z]{3})([-]{1})([0-9]{3})/;
+  let expRegPlateBike = /([A-Z]{3})([-]{1})([0-9]{2})([A-Z]{1})/;
 
-    if (vehicle?.kind === 'Carro' && (parseInt(vehicle?.seats) < 1 || parseInt(vehicle?.seats) > 5)) {
+  if (vehicle?.kind === 'Carro' && (expRegPlateCar.exec(vehicle?.plate) === null)) {
+    toast.error('El Campo Placa Debe Ser Valida',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
 
-        toast.error('El Campo Puesto Debe Estar Entre 1 y 5 Si Es Un Carro',
-            {
-                style: {
-                    borderRadius: '10px',
-                    background: '#282c34',
-                    color: '#2ececece',
-                },
-            }
-        )
-        return false
-    }
+  if (vehicle?.kind === 'Moto' && (expRegPlateBike.exec(vehicle?.plate) === null)) {
+    alert('que pasa')
+    toast.error('El Campo Placa Debe Ser Valida',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
 
-    return true
+  if (vehicle?.model < 1990 || vehicle?.model > 2023) {
+    toast.error('El Campo Modelo debe estar entre 1990 y 2023',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (vehicle?.kind === 'Moto' && !(parseInt(vehicle?.seats) === 1)) {
+    toast.error('El Campo Puesto Solo Puede Ser 1 Si Eliges Una Moto',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (vehicle?.kind === 'Carro' && (parseInt(vehicle?.seats) < 1 || parseInt(vehicle?.seats) > 5)) {
+
+    toast.error('El Campo Cupos Debe Estar Entre 1 y 5 Si Es Un Carro',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  return true
+}
+
+function addMinutes(numOfMinutes, date = new Date()) {
+  date.setMinutes(date.getMinutes() + numOfMinutes);
+
+  return date;
+}
+
+export const validateAddTravel = (travel) => {
+  if (travel?.source === '') {
+    toast.error('El Campo Origen No Puede Estar vacio',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.destiny === '') {
+    toast.error('El Campo Destino No Puede Estar Vacio',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.dateTime < addMinutes(5) || isNaN(travel?.dateTime)) {
+    toast.error('El Campo Fecha Debe Ser Minimo 5 Minutos Más De La Fecha Actual',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if ((travel?.vehicle?.kind === 'Carro' && (travel?.seats < 1 || travel?.seats > 5) || isNaN(travel?.seats)) ) {
+    toast.error('El Campo Cupos Debe Estar Entre 1 y 5 Si Es Un Carro',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.vehicle?.kind === 'Moto' && (travel?.seats !== 1)) {
+    toast.error('El Campo Puesto Solo Puede Ser 1 Si Eliges Una Moto',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.price < 5000 || travel?.price > 20000) {
+    toast.error('El Campo Precio debe estar entre $5000 y $20000',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.contact === '') {
+    toast.error('El Campo Contacto no puede estar vacio',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  if (travel?.vehicle === undefined) {
+    toast.error('Debes Seleccionar Un Vehiculo',
+      {
+        style: {
+          borderRadius: '10px',
+          background: '#282c34',
+          color: '#2ececece',
+        },
+      }
+    )
+    return false
+  }
+
+  return true
 }
