5bb1cebb70021c48053d3637188c07f12372cd57
Joe Mendez <54820626+jzmendezl@users.noreply.github.com>
2022-11-07 20:54:34 -0500

    test3

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(travelsRoutes)
 
 // __dirname = path.resolve()
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
