b50520a4cf078d277d687a7495ed8d0229d41728
Joe Mendez <54820626+jzmendezl@users.noreply.github.com>
2022-11-14 01:24:11 -0500

    fix update data

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -5,17 +5,28 @@ export const getUsersRequest = async () => axios.get('/users')
 
 export const getUserRequest = async (id, token) => axios.get(`/users/${id}`, { headers: { "Authorization": `Bearer ${token}` } })
 
-export const updateUserDataRequest = async (id, userUpdate) => {
-    const form = new FormData();
-    for (const key in userUpdate) {
-        form.append(key, userUpdate[key])            
+export const updateUserDataRequest = async (id, userDataUpdate) => {
+
+    function buildFormData(formData, data, parentKey) {
+        if (data && typeof data === 'object' && !(data instanceof Date) && !(data instanceof File)) {
+            Object.keys(data).forEach(key => {
+                buildFormData(formData, data[key], parentKey ? `${parentKey}[${key}]` : key);
+            });
+        } else {
+            const value = data == null ? '' : data;
+
+            formData.append(parentKey, value);
         }
-        return axios.put(`/users/${id}`, form, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-              },
-        })
     }
+
+    const form = new FormData();
+    buildFormData(form, userDataUpdate)
+    return axios.put(`/users/${id}`, form, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    })
+}
 
 export const updateUserRequest = async (id, userUpdate) => axios.put(`/users/${id}`, userUpdate)
 
